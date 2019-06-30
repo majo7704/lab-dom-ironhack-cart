54ffde86762f3e7e07b73ea1810b23e373ab5d63
j m <jm@js-MacBook-Pro.local>
2019-06-30 22:05:42 +0200

    rendered elements + buttons

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -19,13 +19,18 @@ for (i = 0; i < quantityInputs.length; i ++) {
 }
 let updatePriceButton = document.getElementById('calc-prices-button')
 updatePriceButton.addEventListener("click", updateSubtotalPrice)
+updatePriceButton.addEventListener("click", calculatePrices)
+
+let createButton = document.getElementsByClassName('btn-create')[0]
+createButton.addEventListener('click', displayNewElements)
 }
 
 //FUNCTIONS
 function deleteItem(e) {
-  var buttonClicked = e.target
+  var buttonClicked = e.currentTarget
   buttonClicked.parentNode.parentNode.remove()
   updateSubtotalPrice()
+  calculatePrices()
 }
 
 function quantityChanged (event) {
@@ -34,21 +39,125 @@ function quantityChanged (event) {
     input.value = 1
   }
   updateSubtotalPrice()
+  calculatePrices()
 }
 
 function updateSubtotalPrice (){
   let productsContainers = document.getElementsByClassName('ultimate-container')[0]
-  let rows = productsContainers.getElementsByClassName('product-row')
+  let productRows = productsContainers.getElementsByClassName('product-row')
   let subtotalPrice = 0
-  for (let i = 0; i < rows.length; i ++) {
-    let row = rows[i]
-    let unitPrice = row.getElementsByClassName('unit-box')[0]
-    let priceOfItem = parseFloat(unitPrice.innerText.replace('$', ''))
-    let quantity = row.getElementsByClassName('quantity')[0]  
+  for (let i = 0; i < productRows.length; i ++) {
+    let productRow = productRows[i]
+    let priceElement = productRow.getElementsByClassName('unit-box')[0]
+    let priceOfItem = parseFloat(priceElement.innerText.replace('$', ''))
+    let quantity = productRow.getElementsByClassName('quantity')[0]  
     let inputValue = quantity.value
-    subtotalPrice = subtotalPrice + (priceOfItem * inputValue)
+    let subtotal = document.getElementsByClassName('subtotal')[0].innerText.replace('$', '')
+    
+    subtotalPrice = parseInt(priceOfItem * inputValue)
+    
   }
-  let subtotal = document.getElementsByClassName('subtotal')[0].innerText.replace('$', '')
-    let priceOfUnits = parseFloat(subtotal)
-  console.log(priceOfUnits)
+  subtotalPrice = Math.round(subtotalPrice)
+document.getElementsByClassName('subtotal')[0].innerText = '$' + subtotalPrice;
+
   }
+
+  function calculatePrices () {
+    let productsContainers = document.getElementsByClassName('ultimate-container')[0]
+    let productRows = productsContainers.getElementsByClassName('product-row')
+    for (let i = 0; i < productRows.length; i++) {
+      let productRow = productRows[i]
+      let subtotal = productRow.getElementsByClassName('subtotal')[0].innerText.replace('$', '')
+      
+  }
+}
+
+//creating an element
+let row = {
+  name: '<name>',
+  price: '<price>',
+  quantity: '<quantity>',
+  subtotal: '<subtotal>',
+  button: '<button>'
+};
+typeof row.toString();
+console.log(row);
+
+function Row(name, price, quantity, subtotal, button) {
+  this.name = name;
+  this.price = price;
+  this.quantity= quantity;
+  this.subtotal = subtotal;
+  this.button = button;
+  toString = function () {
+    return 'Name: ' + this.name + ',' +
+      ' price: ' + this.price + ',' + ' quantity ' + this.quantity + ',' +
+      ' subtotal: ' + this.subtotal + ' button ' + this.button
+    }
+  }
+let arrayOfRows = [
+  new Row('IronBubble-head', "25.00", 2, 0.00, "button"),
+  new Row('IronShirt', "15.00", 1, 0.00, "button"),
+  new Row('IronCup', "10.00", 1, 0.00, "button"),
+  new Row('IronSticker', "1.00", 2, 0.00, "button"),
+  new Row('IronAxe', "10.00", 1, 0.00, "button"),
+  new Row('IronPen', "15.00", 1, 0.00, "button"),
+  new Row('IronMug', "30.00", 2, 0.00, "button")
+]
+function RowList (rows) {
+  this.allRows = rows;
+}
+
+function displayNewElements (row) {
+  let displayButton = document.createElement('button');
+  displayButton.className = 'btn btn-delete';
+  displayButton.textContent = 'DELETE';
+  displayButton.addEventListener('click', deleteItem);
+
+  let buttonDiv = document.createElement('div');
+  buttonDiv.appendChild(displayButton);
+
+  let displaySubtotal = document.createElement('span');
+  displaySubtotal.className = 'subtotal';
+  displaySubtotal.textContent = '$ 0.00';
+
+  let subtotalDiv = document.createElement('div');
+  subtotalDiv.appendChild(displaySubtotal);
+
+  let dispalyQuantity = document.createElement('input');
+  dispalyQuantity.className = 'quantity';
+  dispalyQuantity.type = 'number';
+  dispalyQuantity.value = row.quantity;
+  let displayLabel = document.createElement('label');
+  displayLabel.textContent = 'QTY';
+
+  let quantityDiv = document.createElement('div');
+  quantityDiv.className = 'input-container'
+  quantityDiv.appendChild(displayLabel);
+  quantityDiv.appendChild(dispalyQuantity);
+
+  let priceDisplay = document.createElement('span');
+  priceDisplay.className = 'unit-box';
+  priceDisplay.textContent = '$' + row.price;
+  let nameDisplay = document.createElement('span');
+  nameDisplay.className = 'product-title';
+  nameDisplay.textContent = row.name;
+  let priceDiv = document.createElement('div');
+  priceDiv.appendChild(nameDisplay);
+  priceDiv.appendChild(priceDisplay);
+
+  let displayRow = document.createElement('div');
+  displayRow.className = 'product-row'
+  displayRow.appendChild(priceDiv);
+  displayRow.appendChild(quantityDiv);
+  displayRow.appendChild(subtotalDiv);
+  displayRow.appendChild(buttonDiv);
+
+  document.querySelector('.ultimate-container').appendChild(displayRow);
+}
+
+let rows = new RowList();
+for (i = 0; i < arrayOfRows.length; i++) { //Initialization
+  displayNewElements(arrayOfRows[i]);
+}
+
