1e8d7f5e8c8b320cd255c9d6ab2cbe1faf9d7399
j m <jm@js-MacBook-Pro.local>
2019-06-30 07:35:25 +0200

    few functions

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,36 +1,54 @@
+
 if(document.readyState == 'loading') {
   document.addEventListener('DOMContentLoaded', ready)
 } else {
   ready()
+  
 }
+
 function ready(){
-let removeButtons = document.getElementsByClassName('btn-delete');
-
-for (let i = 0; i < removeButtons.length; i++) {
-  var button = removeButtons[i];
-  button.addEventListener("click", function deleteItem(e) {
-  var buttonClicked = e.currentTarget
-  buttonClicked.parentElement.parentElement.remove()
-  calculateTotalPrice()
-})
-
+let deleteButtons = document.getElementsByClassName('btn-delete');
+for (let i = 0; i < deleteButtons.length; i++) {
+  var button = deleteButtons[i];
+  button.addEventListener("click", deleteItem)
 }
+let quantityInputs = document.getElementsByClassName('quantity')
+for (i = 0; i < quantityInputs.length; i ++) {
+  let input = quantityInputs[i]
+  input.addEventListener('change', quantityChanged)
 }
-function calculateTotalPrice (){
-  let productContainer = document.getElementsByClassName('ultimate-container')[0]
-  let productRows = productContainer.getElementsByClassName('product-box')
-  let total = 0
-  debugger;
-  for (var i =0; i < productRows.length; i++){
-    let productRow = productRows[i]
-    let priceProduct = productRow.getElementsByClassName('unit-box')[0]
-    let quantities = productRow.getElementsByClassName('quantity')[0]
-    let price = parseFloat(priceProduct.innerHTML.replace('$', ''))
-    let quantity = quantities.value
-    total =+ (price * quantity)
+let updatePriceButton = document.getElementById('calc-prices-button')
+updatePriceButton.addEventListener("click", updateSubtotalPrice)
+}
+
+//FUNCTIONS
+function deleteItem(e) {
+  var buttonClicked = e.target
+  buttonClicked.parentNode.parentNode.remove()
+  updateSubtotalPrice()
+}
+
+function quantityChanged (event) {
+  let input = event.target
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1
   }
-  let tot = document.getElementsByClassName('subtotal')[0].innerText.replace('$', '')
-  tot = '$' + total
+  updateSubtotalPrice()
 }
-document.getElementById('calc-prices-button').addEventListener("click", calculateTotalPrice());
 
+function updateSubtotalPrice (){
+  let productsContainers = document.getElementsByClassName('ultimate-container')[0]
+  let rows = productsContainers.getElementsByClassName('product-row')
+  let subtotalPrice = 0
+  for (let i = 0; i < rows.length; i ++) {
+    let row = rows[i]
+    let unitPrice = row.getElementsByClassName('unit-box')[0]
+    let priceOfItem = parseFloat(unitPrice.innerText.replace('$', ''))
+    let quantity = row.getElementsByClassName('quantity')[0]  
+    let inputValue = quantity.value
+    subtotalPrice = subtotalPrice + (priceOfItem * inputValue)
+  }
+  let subtotal = document.getElementsByClassName('subtotal')[0].innerText.replace('$', '')
+    let priceOfUnits = parseFloat(subtotal)
+  console.log(priceOfUnits)
+  }
